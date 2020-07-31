e92aa5aa140be8bb543ca4facb62e79e707a7fa3
Franz Hoepfner <kinnarr@hollywuuud.de>
2020-07-31 13:45:38 +0200

    Increase python to 3.8: fixing memory leaks in python async and for dataclasses

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     packages=["metricq", "metricq_proto"],
     scripts=[],
     install_requires=[
